c2442c26c538c90c47b5bced995bad69f6783422
Yara <yaravlvz99@gmail.com>
2022-04-20 22:43:53 +0200

    Añadida documentacion Javadoc al archivo Program

--- Código Documentado/Ejercicio_13/src/ejercicio_13/Program.java
+++ Código Documentado/Ejercicio_13/src/ejercicio_13/Program.java
@@ -5,19 +5,45 @@
 package ejercicio_13;
 
 /**
- *
- * @author dam1d13
+ * @version Program 1.0
+ * @author Yara Velasco Vazquez
+ * @since 17/04/2022
+ */
+/**
+ * Creamos la clase Program
  */
 public class Program 
 {
+	/**
+	 * Metodo vacio para mostrar datos por pantalla
+	 * @param args instrucciones del metodo estatico void del programa
+	 */
     //  Metodo por el cual comenzara a ejecutarse el programa
     public static void main(String[] args)
     {
+    	/**
+    	 * Declaramos la variable termino
+    	 */
         int termino;
+        /**
+         * Creamos el objeto de la clase Sucesiones
+         */
         Sucesiones objfibo= new Sucesiones();
+        /**
+         * Pedimos al usuario que introduzca el numero de iteraciones que se van a mostrar de la sucesion
+         */
         System.out.print("Introduzca un valor: ");
+        /**
+         * Leemos la variable
+         * @see Teclado para realizar lectura de datos
+         */
         termino = Teclado.entero();
+        /**
+         * El objeto invoca al metodo Fibonacci de la clase Sucesiones para pasarle la variable
+         */
         objfibo.Fibonacci(termino);
         
     }
 }
+
+ 
